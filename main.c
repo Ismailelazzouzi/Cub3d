03f4cb2151095df12fdbaa70d6005878ce5fb977
ismail el azzouzi <ismailazzouzi.contact@gmail.com>
2025-01-22 03:55:16 +0100

    checks

--- main.c
+++ main.c
@@ -21,19 +21,42 @@ typedef struct s_data
 }	t_data;
 
 
-void	check_extention(char *filename, char *ext)
+void	free_data(t_data *data, bool flag)
 {
 	int	i;
 
 	i = 0;
-	while (filename[i])
+	if(data->so)
+		free(data->no);
+	if (data->so)
+		free(data->so);
+	if (data->ea)
+		free(data->ea);
+	if (data->we)
+		free(data->we);
+	while (i < data->rows_num)
+		free(data->maze[i++]);
+	if (data->maze)
+		free(data->maze);
+	if (flag == true)
+		printf("INVALID INPUT\n"), exit(1);
+}
+
+void	check_extention(char *filename, char *ext, bool flag, t_data *data)
+{
+	int	namelen;
+	int	extlen;
+
+	namelen = ft_strlen(filename);
+	extlen = ft_strlen(ext);
+	filename = filename + (namelen - extlen);
+	if (ft_strncmp(filename, ext, extlen) != 0)
 	{
-		if (filename[i] == '.')
-			break;
-		i++;
+		printf("SMYA GHALTA AL3ACHIR\n");
+		if (flag == true)
+			free_data(data, false);
+		exit(1);
 	}
-	if (ft_strncmp(filename + i, ext, ft_strlen(filename)) != 0)
-		printf("SMYA DYAL MAP GHALTA AL3ACHIR\n"), exit(1);
 }
 
 void	init_game_data(t_data *data)
@@ -142,22 +165,26 @@ void	get_rows(char *av1, t_data *data)
 
 void	data_entry(char *line, t_data *data, int i)
 {
-	if (line[0] == 'N' && line[1] == 'O' && line[2] == ' ')
-		data->no = ft_strdup(line + 3);
-	else if ((line[0] == 'S' && line[1] == 'O' && line[2] == ' '))
-		data->so = ft_strdup(line + 3);
-	else if (line[0] == 'E' && line[1] == 'A' && line[2] == ' ')
-		data->ea = ft_strdup(line + 3);
-	else if (line[0] == 'W' && line[1] == 'E' && line[2] == ' ')
-		data->we = ft_strdup(line + 3);
-	else if (line[0] == 'C' && line[1] == ' ')
-		parse_colors(line + 2, data->c);
-	else if (line[0] == 'F' && line[1] == ' ')
-		parse_colors(line + 2, data->f);
+	char	*trimmed_line;
+
+	trimmed_line = ft_strtrim(line, " \t\v\f\r\n");
+	if (trimmed_line[0] == 'N' &&trimmed_line[1] == 'O' &&trimmed_line[2] == ' ')
+		data->no = ft_strdup(trimmed_line + 3);
+	else if (trimmed_line[0] == 'S' && trimmed_line[1] == 'O' && trimmed_line[2] == ' ')
+		data->so = ft_strdup(trimmed_line + 3);
+	else if (trimmed_line[0] == 'E' && trimmed_line[1] == 'A' &&trimmed_line[2] == ' ')
+		data->ea = ft_strdup(trimmed_line + 3);
+	else if (trimmed_line[0] == 'W' && trimmed_line[1] == 'E' && trimmed_line[2] == ' ')
+		data->we = ft_strdup(trimmed_line + 3);
+	else if (trimmed_line[0] == 'C' && trimmed_line[1] == ' ')
+		parse_colors(trimmed_line + 2, data->c);
+	else if (trimmed_line[0] == 'F' && trimmed_line[1] == ' ')
+		parse_colors(trimmed_line + 2, data->f);
 	else if (line[0] == '\n')
 		line[1] = '\0';
 	else
 		data->maze[i] = ft_strdup(line);
+	free(trimmed_line);
 }
 
 void	parse_file(char *av1, t_data *data)
@@ -185,6 +212,35 @@ void	parse_file(char *av1, t_data *data)
 	close(fd);
 }
 
+void	check_input(t_data *data)
+{
+	int	fd[4];
+	int	i;
+
+	i = 0;
+	if (!data->so || !data->no || !data->ea || !data->we)
+		free_data(data, true);
+	check_extention(data->no, ".png", true, data);
+	check_extention(data->so, ".png", true, data);
+	check_extention(data->ea, ".png", true, data);
+	check_extention(data->we, ".png", true, data);
+	fd[0] = open(data->no, O_RDONLY);
+	fd[1] = open(data->so, O_RDONLY);
+	fd[2] = open(data->ea, O_RDONLY);
+	fd[3] = open(data->we, O_RDONLY);
+	while (i < 4)
+	{
+		if (fd[i] < 0)
+		{
+			free_data(data, true);
+			break ;
+		}
+		i++;
+	}
+	while (i >= 0)
+		close(fd[i--]);
+}
+
 int main(int argc, char **argv)
 {
 	t_data	data;
@@ -192,12 +248,9 @@ int main(int argc, char **argv)
 	if (argc != 2)
 		printf("ARGUMENTS GHALAT A BRO/SIS\n"), exit(1);
 	if (argv[1])
-		check_extention(argv[1], ".cub");
+		check_extention(argv[1], ".cub", false, &data);
 	init_game_data(&data);
 	parse_file(argv[1], &data);
-	int i = 0;
-	printf("NO %s\nSO %s\nWE %s\nEA %s\n\nF %d, %d, %d\nC %d, %d, %d\n\n", data.no, data.so, data.ea, data.we, data.c[0], data.c[1], data.c[2], data.f[0], data.f[1], data.f[2]);
-	while (data.maze[i])
-		printf("%s", data.maze[i++]);
+	check_input(&data);
 	return (0);
 }
